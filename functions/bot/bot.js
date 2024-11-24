22848ef2cabc2ef2504de29b0c00c5f6e127fe44
Ministro97 <74841571+Ministro97@users.noreply.github.com>
2024-11-24 23:31:31 +0100

    Update bot.js

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -474,24 +474,15 @@ async function sendSummary(ctx) {
 
 
 
+//AWS handler 
+exports.handler = async event => {
+  try {
+    await bot.handleUpdate(JSON.parse(event.body));
+    return { statusCode: 200, body: "" };
+  } catch (e) {
+    console.error("error in handler:", e);
+    return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" };
+  }
+};
 
 
-
-
-bot.launch();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log('Bot avviato...');
