7ba518bb33082570da786b6d3f3c01fa2c827d98
Ministro97 <74841571+Ministro97@users.noreply.github.com>
2024-11-29 01:36:39 +0100

    Update bot.js

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -67,7 +67,7 @@ bot.use(async(ctx, next) => {
 
 // Funzione per salvare i prefissi validi in un file JSON 
 function saveValidPrefixes(prefixes) {
-    const filePath = path.join('tmp','/tmp/validPrefixes.json');
+    const filePath = path.join('tmp','validPrefixes.json');
     const data = JSON.stringify({ validPrefixes: prefixes }, null, 2);
     fs.writeFileSync(filePath, data);
 }
