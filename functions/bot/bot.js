9333a02b854cd2f9f77c623e3c240bd624f067ec
Ministro97 <74841571+Ministro97@users.noreply.github.com>
2024-12-02 01:04:33 +0100

    Update bot.js

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -45,7 +45,8 @@ const step2 = async (ctx) => {
 const predefinedMessage = 'Questo è un messaggio predefinito per il topic.';
 
 // Invio del messaggio predefinito al topic appena creato
-await ctx.telegram.sendMessage(ctx.chat.id, predefinedMessage, { message_thread_id: topicMessage.message_id });
+await ctx.telegram.sendMessage(ctx.chat.id, predefinedMessage, { message_thread_id: topicMessage.message_id + 1 });
+  console.log (topicMessage.message_id)  
 
   } catch (error) {
     console.error(error);
