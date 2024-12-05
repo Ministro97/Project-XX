75ec0e82b781e49c6ff6936943af02347b046121
Ministro97 <74841571+Ministro97@users.noreply.github.com>
2024-12-05 13:48:20 +0100

    Update bot.js

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1143,9 +1143,9 @@ const client = new Client({
     // Query per trovare l'utente e poi aggiorna l'idea +1
 
 
-             // let lastIdea = 1609
+              let lastIdea = 1609
     const userQuery = fql`
-        Users.where(.ideaId == ${ideaId}) //ideaId
+        Users.where(.ideaId == ${lastIdea}) //ideaId
     `;
     const user = await client.query(userQuery);
 
