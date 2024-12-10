5fb8c781aa818f67da533d46467259c0d308f0e3
Ministro97 <74841571+Ministro97@users.noreply.github.com>
2024-12-10 18:39:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    const userId = getTelegramUserId();
-    if (userId) {
-        document.body.innerHTML += `<p>ID Utente: ${userId}</p>`;
-    } else {
-        document.body.innerHTML += `<p>Impossibile ottenere l'ID utente.</p>`;
+function getTelegramUserId() {
+    if (window.Telegram && window.Telegram.WebApp && window.Telegram.WebApp.initDataUnsafe) {
+        return window.Telegram.WebApp.initDataUnsafe.user.id;
     }
+    return null;
+}
 
-    // Codice per la gestione dei pulsanti
-    const buttons = document.querySelectorAll(".card-buttons button");
-    const sections = document.querySelectorAll(".card-section");
-    const card = document.querySelector(".card");
 
-    const handleButtonClick = e => {
-        const targetSection = e.target.getAttribute("data-section");
-        const section = document.querySelector(targetSection);
 
-        targetSection !== "#about" ?
-        card.classList.add("is-active") :
-        card.classList.remove("is-active");
+const buttons = document.querySelectorAll(".card-buttons button");
+const sections = document.querySelectorAll(".card-section");
+const card = document.querySelector(".card");
 
-        card.setAttribute("data-state", targetSection);
+const handleButtonClick = e => {
+    const targetSection = e.target.getAttribute("data-section");
+    const section = document.querySelector(targetSection);
 
-        sections.forEach(s => s.classList.remove("is-active"));
-        buttons.forEach(b => b.classList.remove("is-active"));
+    targetSection !== "#about" ?
+    card.classList.add("is-active") :
+    card.classList.remove("is-active");
 
-        e.target.classList.add("is-active");
-        section.classList.add("is-active");
-    };
+    card.setAttribute("data-state", targetSection);
 
-    buttons.forEach(btn => {
-        btn.addEventListener("click", handleButtonClick);
-    });
+    sections.forEach(s => s.classList.remove("is-active"));
+    buttons.forEach(b => b.classList.remove("is-active"));
+
+    e.target.classList.add("is-active");
+    section.classList.add("is-active");
+};
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", handleButtonClick);
 });
