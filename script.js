954c6cdacb9cd78bbc11f1b6ff648742dfb86cca
Ministro97 <74841571+Ministro97@users.noreply.github.com>
2024-12-12 22:55:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,104 @@ const handleButtonClick = e => {
 buttons.forEach(btn => {
     btn.addEventListener("click", handleButtonClick);
 });
+
+
+
+
+
+
+
+
+
+
+const messageElement = document.getElementById('message');
+        const token = sessionStorage.getItem('authToken');
+
+        if (token) {
+            // Verifica il token
+            if (token) { // Se il token esiste, l'autenticazione è riuscita
+
+                window.Telegram.WebApp.ready();
+
+document.getElementById('user_card').style.display = "block";
+
+
+
+
+
+
+                
+            } else {
+                messageElement.textContent = 'Token non valido. Autenticazione fallita.';
+                messageElement.classList.add('error');
+            }
+        } else {
+            messageElement.textContent = 'Nessun token fornito. Autenticazione fallita.';
+            messageElement.classList.add('error');
+        }
+
+
+
+
+document.querySelectorAll('.card-contact').forEach(function(contact) {
+  contact.addEventListener('mouseover', function() {
+    const tooltip = this.querySelector('.tooltip');
+    tooltip.style.display = 'block';
+    tooltip.style.opacity = '1';
+
+    setTimeout(function() {
+      tooltip.style.opacity = '0';
+      setTimeout(function() {
+        tooltip.style.display = 'none';
+      }, 500); // Tempo per il fade out
+    }, 1000); // Tempo di visualizzazione del tooltip
+  });
+});
+
+  
+  
+  
+  
+  
+  function toggleVisibility() {
+            const masked = document.querySelector('.hidden-code .masked');
+            const unmasked = document.querySelector('.hidden-code .unmasked');
+            const eyeIcon = document.getElementById('eyeIcon');
+            if (masked.style.display === 'none') {
+                masked.style.display = 'inline';
+                unmasked.style.display = 'none';
+                eyeIcon.classList.remove('fa-eye');
+                eyeIcon.classList.add('fa-eye-slash');
+            } else {
+                masked.style.display = 'none';
+                unmasked.style.display = 'inline';
+                eyeIcon.classList.remove('fa-eye-slash');
+                eyeIcon.classList.add('fa-eye');
+            }
+        }
+  
+  
+  document.addEventListener('DOMContentLoaded', (event) => {
+  const card = document.getElementById('card');
+
+  window.addEventListener('deviceorientation', (event) => {
+    const { beta, gamma } = event; // beta: tilt front-back, gamma: tilt left-right
+
+    // Limita i valori per evitare movimenti eccessivi
+    const maxTilt = 15;
+    const tiltX = Math.min(Math.max(gamma, -maxTilt), maxTilt);
+    const tiltY = Math.min(Math.max(beta, -maxTilt), maxTilt);
+
+    // Applica la trasformazione alla carta
+    card.style.transform = `rotateX(${tiltY}deg) rotateY(${tiltX}deg)`;
+  });
+});
+
+
+
+
+
+
+
+
+
