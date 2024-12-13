4942dbab95b96ada2958a096138780fd4c49a96e
Ministro97 <74841571+Ministro97@users.noreply.github.com>
2024-12-13 02:38:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function getTelegramUserId() {
 
 const userId = getTelegramUserId();
     if (userId) {
-        document.getElementById("userName").testContent = window.Telegram.WebApp.initDataUnsafe.user.first_name;
+        document.getElementById("userName").textContent = window.Telegram.WebApp.initDataUnsafe.user.first_name;
         document.getElementById("userId").textContent = userId;
         document.getElementById("deviceId").textContent = sessionStorage.getItem('deviceId');
         document.getElementById("key").textContent = sessionStorage.getItem('authToken');
